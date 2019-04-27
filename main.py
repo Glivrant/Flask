b836091a6e3645f99d09c6071b1104a3cf93dd74
Glivrant <49487658+Glivrant@users.noreply.github.com>
2019-04-27 17:44:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def hello():
     return "n the beginning tof the year 1800 Volta tmade the first electric battery. He made it of tcopper and zink disks whi'dry battery'."
 
-result = re.findall(t)
+result = re.findall(r't\w*'string)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
